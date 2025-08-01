dcea9e58cae068aed2b674443248fecf1336fa6c
reymagdalena <magdalena.rey@estudiantes.utec.edu.uy>
2025-08-01 00:05:18 -0300

    segundo commit

--- src/main/java/com/utec/repository/PerfilRepository.java
+++ src/main/java/com/utec/repository/PerfilRepository.java
@@ -2,6 +2,8 @@ package com.utec.repository;
 
 import com.utec.model.Perfil;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -9,4 +11,9 @@ import java.util.Optional;
 @Repository
 public interface PerfilRepository extends JpaRepository <Perfil, Integer> {
     Optional<Perfil> findByNombre(String administrador);
+
+    @Query("SELECT COUNT(f) > 0 FROM Perfil p JOIN Funcionalidad f " +
+            "WHERE p.idPerfil = :perfilId AND f.nombre = :funcionalidad")
+    boolean existeFuncionalidadParaPerfil(@Param("perfilId") Integer perfilId,
+                                          @Param("funcionalidad") String funcionalidad);
 }
